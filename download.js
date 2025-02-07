1a60a5c37f0db018277815287306697875e00f53
oxonomy14 <oxonomy14@gmail.com>
2025-02-07 17:44:16 +0000

    Deploying to gh-pages from @ ingenium-life/exprates@9e1ffcdc9a325f2ac5a9a399be41d788ff4407ef 🚀

---
+++ download.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import puppeteer from 'puppeteer';
+import path from 'path';
+
+// Получаем путь к текущей директории
+const directoryPath = path.join(new URL(import.meta.url).pathname);
+
+// Если директория не существует, создаём её
+if (!fs.existsSync(directoryPath)) {
+  fs.mkdirSync(directoryPath, { recursive: true });
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
+  });
+
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+  );
+  await page.goto('https://vipchanger.com/res/xml/exprates.xml');
+
+  // Используем page.content(), чтобы получить исходный контент страницы (включая XML)
+  const xmlContent = await page.content();
+
+  // Путь к файлу внутри src
+  const filePath = path.join(directoryPath, 'exprates.xml');
+
+  // Записываем XML в файл
+  fs.writeFileSync(filePath, xmlContent);
+
+  // Записываем обновлённую метку времени в data.xml
+  fs.writeFileSync(
+    path.join(directoryPath, 'data.xml'),
+    `<updated>${new Date().toISOString()}</updated>`
+  );
+
+  console.log('XML скачан успешно!');
+  await browser.close();
+})();
